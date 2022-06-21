bd3736c454a4d4cc20e80fd43155548354184022
Antonio Sabino <aslimajr@outlook.com>
2022-06-21 11:57:43 -0300

    feat: criação do Controller Product e sua função getAll

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/product.service';
+
+const getAll = async (_req: Request, res: Response) => {
+  const products = await ProductService.getAll();
+
+  res.status(200).json(products);
+};
+
+const ProductController = { getAll };
+
+export default ProductController;
