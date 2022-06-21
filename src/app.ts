050804f171fe55afce957c89d227d8884dda56fa
Antonio Sabino <aslimajr@outlook.com>
2022-06-21 12:01:26 -0300

    feat: implementando a rota /products no app.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import productsRoute from './routes/products.route';
 
 const app = express();
 
 app.use(express.json());
+
+// app.use('/login');
+// app.use('/orders');
+app.use('/products', productsRoute);
+// app.use('/users');
 
 export default app;
